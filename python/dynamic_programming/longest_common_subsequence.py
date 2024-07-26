c699d48c46cd8fa8b3cfb0e077eeb0ccafba0f5a
SirSaleh <animatorsaleh@gmail.com>
2024-07-26 13:28:01 +0330

    Add LCS starting from first order

--- python/dynamic_programming/longest_common_subsequence.py
+++ python/dynamic_programming/longest_common_subsequence.py
@@ -1,12 +1,8 @@
 # Longest Common Subsequence (LCS)
 
 class Solution(object):
-    def longestCommonSubsequence(self, text1, text2):
-        """
-        :type text1: str
-        :type text2: str
-        :rtype: int
-        """
+
+    def from_bottom(self, text1, text2):
         memo = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]        
         
         for i in range(len(text1) - 1, -1, -1):
@@ -17,6 +13,35 @@ class Solution(object):
                     memo[i][j] = max(memo[i][j+1], memo[i+1][j])
                 print(i, j, memo)
         return memo[0][0]
+    
+    def from_top(self, text1, text2):
+        """
+            - - a g f
+            - 0 0 0 0  
+            b 0
+            g 0
+            h 0
+            f 0
+        """
+        memo = [[0] * (len(text2) + 1) for x in range(len(text1) + 1)]
+
+        for i in range(len(text1)):
+            for j in range(len(text2)):
+                if text1[i] == text2[j]:
+                    memo[i+1][j+1] = 1 + memo[i][j]
+                else:
+                    memo[i+1][j+1] = max(memo[i][j+1], memo[i+1][j])
+        return memo[len(text1)][len(text2)]
+
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        #self.from_bottom(text1, text2)
+        return self.from_top(text1, text2)
+        
         
 
 if __name__=="__main__":
